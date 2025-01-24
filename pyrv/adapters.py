9214fec1247d87331af3bff72dbb7ab2aa1cef59
Matias Silva <git@matiasilva.com>
2025-01-24 19:19:16 +0000

    Update assembly adapter

--- pyrv/adapters.py
+++ pyrv/adapters.py
@@ -6,18 +6,47 @@ parse input data and output pyrv instruction objects
 import re
 
 from pyrv.helpers import InvalidInstructionError
-from pyrv.instructions import ITYPE_OPS, OP2INSTR, RTYPE_OPS, Instruction, IType, RType
+from pyrv.instructions import (
+    BTYPE_OPS,
+    ITYPE_OPS,
+    JTYPE_OPS,
+    OP2INSTR,
+    RTYPE_OPS,
+    STYPE_OPS,
+    UTYPE_OPS,
+    BType,
+    Instruction,
+    IType,
+    JType,
+    RType,
+    SType,
+    UType,
+)
 
 OP_RE = re.compile(r"^\s*(\w+)(.*)$")
 
 
 def asm2instr(asm: tuple) -> Instruction:
+    """
+    Convert a string representation of an instruction to a `pyrv` Instruction model.
+
+    This is valid for debug only, compliance with how the assembler compiles
+    assembly files is not guaranteed.
+    """
     op, args = asm
 
     if op in ITYPE_OPS:
         frame = IType(rd=args[0], rs1=args[1], imm=args[2])
     elif op in RTYPE_OPS:
         frame = RType(rd=args[0], rs1=args[1], rs2=args[2])
+    elif op in BTYPE_OPS:
+        frame = BType(rs1=args[0], rs2=args[1], imm=args[2])
+    elif op in JTYPE_OPS:
+        frame = JType(rd=args[0], imm=args[1])
+    elif op in STYPE_OPS:
+        frame = SType(rs1=args[0], rs2=args[1], imm=args[2])
+    elif op in UTYPE_OPS:
+        frame = UType(rd=args[0], imm=args[1])
     else:
         raise InvalidInstructionError
 
@@ -25,12 +54,16 @@ def asm2instr(asm: tuple) -> Instruction:
 
 
 def parse_asm(line: str) -> tuple | None:
+    """
+    Parse an assembly file for instructions, returning the operation and any arguments.
+
+    This is valid for debug only, there is no advanced calculation of label offsets, no
+    notion of linker relaxations, and so on.
+    """
     if m := OP_RE.match(line):
         op = m.group(1)
         args = m.group(2)
-        if (i := args.find("#")) > -1:
+        if (i := args.find("#")) > -1:  # strip comments
             args = args[:i]
         args = [a.strip() for a in args.split(",")]
         return (op, args)
-    else:
-        return None
