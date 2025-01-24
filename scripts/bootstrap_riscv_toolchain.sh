a07d85717e93f8fa6de7347002e356f6571b092a
Matias Silva <git@matiasilva.com>
2025-01-24 12:50:42 +0000

    Update CI script

--- scripts/bootstrap_riscv_toolchain.sh
+++ scripts/bootstrap_riscv_toolchain.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 DOWNLOAD_PATH="toolchain.tar.gz"
 
-wget ${RISCV_TOOLCHAIN_URL} -O ${DOWNLOAD_PATH}
+wget -nv ${RISCV_TOOLCHAIN_URL} -O ${DOWNLOAD_PATH}
 tar -xzf ${DOWNLOAD_PATH} bin
-rm riscv32-unknown-elf-gdb riscv32-unknown-elf-lto-dump # remove unnecessarily large utils
+rm bin/riscv32-unknown-elf-gdb bin/riscv32-unknown-elf-lto-dump # remove unnecessarily large utils
 
