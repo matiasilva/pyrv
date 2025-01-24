a47af26f1aa8c291da928d282a5785861558a42d
Matias Silva <git@matiasilva.com>
2025-01-24 22:10:07 +0000

    Fix boostrap script

--- scripts/bootstrap_riscv_toolchain.sh
+++ scripts/bootstrap_riscv_toolchain.sh
@@ -7,6 +7,5 @@ DOWNLOAD_PATH="toolchain.tar.gz"
 mkdir build
 cd build
 wget -nv ${RISCV_TOOLCHAIN_URL} -O ${DOWNLOAD_PATH}
-tar -xzf ${DOWNLOAD_PATH} bin
-rm bin/riscv32-unknown-elf-gdb bin/riscv32-unknown-elf-lto-dump # remove unnecessarily large utils
-
+tar -xzf ${DOWNLOAD_PATH}
+rm -rf bin/riscv32-unknown-elf-gdb bin/riscv32-unknown-elf-lto-dump share ${DOWNLOAD_PATH}
